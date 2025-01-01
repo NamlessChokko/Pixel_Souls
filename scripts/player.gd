e6c5df278ad0241d92097ecde62ffffdd547154b
ARTempest <164294705+ARTempest@users.noreply.github.com>
2025-01-01 01:53:07 -0500

    Added a little tileset
    Added a little tile set to make it easier to visualize and also to make some collision tests

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,75 +1,94 @@
 extends CharacterBody2D
 
-
+# Node References
 @onready var sprite : AnimatedSprite2D = $AnimatedSprite2D
-@onready var duration: Timer = $Dash/duration
+@onready var duration: Timer = $Movement/Dash/duration
 
-var isDashing : bool = false
+# Actualizable on process variables
+var direction_x : int
+var direction_y : int
+var shift  : bool
+var attack : bool
+
+# Variables actualizable on functions
 var direction : Vector2
+var isDashing : bool = false
+var isAttacking
 
+# Constants
 const SPEED : float = 100.0
 const DASH_SPEED_MULTIPLIER : float = 2.0
 
+
+# Functions
 func _ready() -> void:
-	duration.connect("timeout", durationTimeout)
+	duration.connect("timeout", durationDashTimeout)
+	sprite.get_animation
 
 func _physics_process(delta: float) -> void:
-	var direction_x := Input.get_axis("analog_left", "analog_right")
-	var direction_y := Input.get_axis("analog_up", "analog_down")
-	var shift : bool = Input.is_action_just_pressed("shift")
-	
-	if not isDashing:
-		walkAction(direction_x, direction_y)
-		turnDirectionAction(direction_x)
-		
-		if shift:
-			dashAction(direction_x,direction_y)
-			
-	else:
-		velocity = direction.normalized()  * SPEED * DASH_SPEED_MULTIPLIER
+	direction_x = Input.get_axis("analog_left", "analog_right")
+	direction_y = Input.get_axis("analog_up", "analog_down")
+	shift = Input.is_action_just_pressed("shift")
+	attack = Input.is_action_just_pressed("attack")
 
+	if not isDashing and not isAttacking:
+		walkAction()
+		turnDirectionAction()
+		if shift:
+			dashAction()
+		elif attack:
+			attackAction()
+	elif isDashing:
+		velocity = direction.normalized()  * SPEED * DASH_SPEED_MULTIPLIER
+	elif isAttacking:
+		pass
+	
 	
 	move_and_slide()
 
-func walkAction(dir_x : float,dir_y : float) -> void:
-	if dir_x or dir_y:
-		velocity = Vector2(dir_x,dir_y).normalized() * SPEED
+
+func walkAction() -> void:
+	if direction_x or direction_y:
+		velocity = Vector2(direction_x,direction_y).normalized() * SPEED
 		sprite.play("walking")
-	if not dir_x:
+	if not direction_x:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-	if not dir_y:
+	if not direction_y:
 		velocity.y = move_toward(velocity.y, 0, SPEED)
 	if not isWalking():
 		sprite.play("idle")
 
-func turnDirectionAction(dir_x : float) -> void:
-	if dir_x > 0:
+func turnDirectionAction() -> void:
+	if direction_x > 0:
 		sprite.flip_h = false
-	elif dir_x < 0:
+	elif direction_x < 0:
 		sprite.flip_h = true
 
-func changeDashDirection(dir_x : int,dir_y : int):
+func calculateDirection():
 	if isWalking():
-		direction = Vector2(dir_x,dir_y)
+		direction = Vector2(direction_x,direction_y)
 	else:
 		if sprite.flip_h == true:
 			direction = Vector2(-1,0)
 		else:
 			direction = Vector2(1,0)
 
-func dashAction(dir_x : int, dir_y : int) -> void:
-	changeDashDirection(dir_x,dir_y)
+func isWalking() -> bool:
+	return true if velocity != Vector2(0,0) else false
+
+func dashAction() -> void:
+	calculateDirection()
 	sprite.modulate = "ffffff99"
 	duration.start()
 	sprite.play("walking")
 	sprite.speed_scale = 3
 	isDashing = true
-	
 
-func isWalking() -> bool:
-	return true if velocity != Vector2(0,0) else false
-
-func durationTimeout() -> void:
+func durationDashTimeout() -> void:
 	sprite.modulate = "ffffff"
 	sprite.speed_scale = 1
 	isDashing = false
+
+func attackAction():
+	isAttacking = true
+	sprite.play("attacking")
